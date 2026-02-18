3e3d6669fd7f0d75833abf2b595214a9f064934a
Fiska Yeh <fiskayeh@Fiskas-Mac-mini.local>
2026-02-19 02:47:04 +0800

    hot fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 如果你的 GitHub 倉庫名稱是 'haewon-quiz'，請將 base 改為 '/haewon-quiz/'
+  // 使用 './' 可以讓打包後的檔案使用相對路徑載入，最適合 GitHub Pages
   base: './', 
 });
