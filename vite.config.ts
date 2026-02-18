12ffa421091b79fe546fe7e77887cbc3356c671e
Fiska Yeh <fiskayeh@Fiskas-Mac-mini.local>
2026-02-19 02:52:58 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 使用 './' 可以讓打包後的檔案使用相對路徑載入，最適合 GitHub Pages
-  base: './', 
+  base: './',
 });
